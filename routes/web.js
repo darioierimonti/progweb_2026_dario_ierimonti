9547902c6d7827b992b226331c91f895c26852e2
Dario Ierimonti <ierimonti@a-piu.it>
2026-04-26 21:06:11 +0200

    static routing support added

--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// Uncomment to use dynamic routes instead of static routes
+
+/*
+router.get('/', (req, res) => {
+    res.send('Welcome');
+});
+ */
+
 module.exports = router;
