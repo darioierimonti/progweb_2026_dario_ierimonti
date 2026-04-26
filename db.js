bb84e530a29b14d882f44f1ba40b98f26c7161e7
Dario Ierimonti <ierimonti@a-piu.it>
2026-04-26 17:24:39 +0200

    set default values for db connection and fixed missing await in query

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || '',
     waitForConnections: true,
     connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
     queueLimit: process.env.DB_QUEUE_LIMIT || 0
 }).promise();
 
 async function query(sql, values) {
-    const [rows] = pool.execute(sql, values);
+    const [rows] = await pool.execute(sql, values);
     return rows;
 }
 
