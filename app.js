a4ab61c431dcf5b666da8537065d923b5643bc21
Dario Ierimonti <ierimonti@a-piu.it>
2026-04-26 16:27:20 +0200

    basic routing

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.APP_PORT;
 
-app.get('/', (req, res) => {
-    res.send('Welcome');
-});
+app.use(express.json());
+
+/**
+ * load routes
+ */
+app.use('/', require('./routes/web'));
+app.use('/api', require('./routes/api'));
 
 app.listen(port, () => {
     console.log(`Server listening on http://127.0.0.1:${port}/`);
